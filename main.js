6375f42f29d1a9ba5d584f1aaa9294c2da27fc39
Gui Prada <guiprada@gmail.com>
2025-05-05 22:38:23 -0300

    - Added grid test

--- main.js
+++ main.js
@@ -29,4 +29,28 @@ UI.addTextNode(stack_3, "Paragrafo 3").style.background = getRandomColor();
 //
 let grid = UI.addGrid(workspace);
 UI.addTextNode(grid, "________");
+UI.addTextNode(grid, "Paragrafo 1").style.background = getRandomColor();
+UI.addTextNode(grid, "Paragrafo 2").style.background = getRandomColor();
+UI.addTextNode(grid, "Paragrafo 3").style.background = getRandomColor();
+UI.addTextNode(grid, "Paragrafo 4").style.background = getRandomColor();
+UI.addTextNode(grid, "Paragrafo 5").style.background = getRandomColor();
+UI.addTextNode(grid, "Paragrafo 6").style.background = getRandomColor();
+UI.addTextNode(grid, "Paragrafo 7").style.background = getRandomColor();
+UI.addTextNode(grid, "Paragrafo 8").style.background = getRandomColor();
+UI.addTextNode(grid, "Paragrafo 9").style.background = getRandomColor();
+UI.addTextNode(grid, "Paragrafo 10").style.background = getRandomColor();
+UI.addTextNode(grid, "Paragrafo 11").style.background = getRandomColor();
+UI.addTextNode(grid, "Paragrafo 12").style.background = getRandomColor();
+UI.addTextNode(grid, "Paragrafo 13").style.background = getRandomColor();
+UI.addTextNode(grid, "Paragrafo 14").style.background = getRandomColor();
+UI.addTextNode(grid, "Paragrafo 15").style.background = getRandomColor();
+UI.addTextNode(grid, "Paragrafo 16").style.background = getRandomColor();
+UI.addTextNode(grid, "Paragrafo 17").style.background = getRandomColor();
+UI.addTextNode(grid, "Paragrafo 18").style.background = getRandomColor();
+UI.addTextNode(grid, "Paragrafo 19").style.background = getRandomColor();
+UI.addTextNode(grid, "Paragrafo 20").style.background = getRandomColor();
+UI.addTextNode(grid, "Paragrafo 21").style.background = getRandomColor();
+UI.addTextNode(grid, "Paragrafo 22").style.background = getRandomColor();
+UI.addTextNode(grid, "Paragrafo 23").style.background = getRandomColor();
+
 
